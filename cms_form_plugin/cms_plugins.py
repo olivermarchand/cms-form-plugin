73abeda167100ccde26b8da1465771d6174d8f22
olivermarchand <oliver.marchand@gmail.com>
2014-09-18 23:46:24 +0200

    pickle had problems with special characters (3)

--- cms_form_plugin/cms_plugins.py
+++ cms_form_plugin/cms_plugins.py
@@ -86,7 +86,7 @@ class CMSFormPlugin(CMSPluginBase):
             form.invalid_url = request.POST['invalid_url']
             delattr(form, 'request')
             request.session[
-                'invalid_form_%s' % instance_id] = pickle.dumps(form, 1)
+                'invalid_form_%s' % instance_id] = pickle.dumps(form, 2)
 
         if not response:
             response = HttpResponseRedirect(
